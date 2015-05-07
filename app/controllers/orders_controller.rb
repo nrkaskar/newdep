3a794240e322bb02e32e87c01c1c01551db1fb8a
askar <askar.nurkey@is.sdu.edu.kz>
2015-05-07 17:32:16 -0500

    abc

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -41,7 +41,7 @@ class OrdersController < ApplicationController
         session[:cart_id] = nil
         OrderNotifier.received(@order).deliver
         format.html { redirect_to store_url, notice: 
-          'Thank you for your order.' }
+          I18n.t('.thanks')  }
         format.json { render json: @order.errors, status: :unprocessable_entity }
       else
         format.html { render action: 'new' }
